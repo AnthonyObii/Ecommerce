ab98f5d6da82fb9bdd85824ab8e2111ad0606d48
AnthonyObii <tonyobi98@gmail.com>
2023-10-14 14:46:14 +0100

    4th

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-    url: env("RENDER_EXTERNAL_URL"),
-    dirs: {
-      public: "/data/public"
-    },
-  });
+  proxy: true,
+  url: env('HOST_URL'), // Sets the public URL of the application.
+  app: {
+    keys: env.array('APP_KEYS')
+  },
+});
