f3a3de53514375293cdcf4c3db3ed5eb5fc9872f
AnthonyObii <tonyobi98@gmail.com>
2023-11-10 10:23:27 +0100

    production

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -23,7 +23,7 @@ const ItemDetails = () => {
 
   async function getItem() {
     const item = await fetch(
-      `http://localhost:1337/api/items/${itemId}?populate=image`,
+      `https://dbfw.onrender.com/api/items/${itemId}?populate=image`,
       { method: "GET" }
     );
     const itemJson = await item.json();
@@ -32,7 +32,7 @@ const ItemDetails = () => {
 
   async function getItems() {
     const items = await fetch(
-      `http://localhost:1337/api/items/?populate=image`,
+      `https://dbfw.onrender.com/api/items/?populate=image`,
       {
         method: "GET",
       }
